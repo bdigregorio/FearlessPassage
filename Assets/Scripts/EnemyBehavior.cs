5ec7c18ed87ef6dc9d121ad31e0c7a26c8743bc1
Benjamin DiGregorio <ben@digregor.io>
2021-11-12 21:24:02 -0800

    Trigger player explosion fx on collision

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    private void OnParticleCollision(GameObject other) {
+    void OnParticleCollision(GameObject other) {
         Debug.Log($"{this.name} has been triggered by {other.gameObject.name}");
+        Destroy(this.gameObject);
+    }
+
+    void OnTriggerEnter(Collider other) {
         Destroy(this.gameObject);
     }
 }
